acb1c5a95f9c357ce4b19ac019a5df011ce7e6cf
rodrigodasilva <ribeiro.rodrigodasilva>
2020-05-14 16:35:14 -0300

    feat: delete transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+
+import AppError from '../errors/AppError';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('This transaction do not exists', 400);
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
